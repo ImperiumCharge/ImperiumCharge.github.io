028a3a070af9eb04d12d3b87676a80fc054da630
ImperiumCharge <61029065+ImperiumCharge@users.noreply.github.com>
2020-02-13 14:50:36 -0800

    deleted extra closing tag
    deleted extra closing tag

--- blocksform.php
+++ blocksform.php
@@ -3,10 +3,10 @@
 if(isset($_POST['submit'])){
     $email = $_POST['Email Address'];
 
-    $mailTo = "imperiumcharge@gmail.com";
-    $headers = "MVP 1 Submission"
+    $to = "imperiumcharge@gmail.com";
+    $subject = "MVP 1 Submission";
 
-    mail($mailTo, $, )
+    //if ( mail( $to, $subject, ))
 }
 
 
