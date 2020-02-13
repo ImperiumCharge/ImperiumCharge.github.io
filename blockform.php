96742b430ef24a97769dd724d9bbd0df9a2a8dd1
ImperiumCharge <61029065+ImperiumCharge@users.noreply.github.com>
2020-02-13 15:30:51 -0800

    added quotes
    added quotes

--- blockform.php
+++ blockform.php
@@ -2,15 +2,14 @@
 
 if(isset($_POST['submit'])){
 
-    $email = $_POST['Email Address'];
-
+    $email = $_POST['email_address'];
     $to = "imperiumcharge@gmail.com";
     $subject = "MVP 1 Submission";
     $message = "it works!";
     $echo_result = "sent";
 
-    if ( mail($to, $subject, $message) ) {
-        echo "Sent!";
+    if ( mail("$to", $subject, $message) ) {
+        echo "Sent";
     } else {
         echo "Did not send";
     }
